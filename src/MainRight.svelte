16c1622e5313ef1e66bfd2d0ade605cd1dfd1a3d
Dan <dan@mail.com>
2020-06-17 20:12:28 +0200

    WIP create post

--- src/MainRight.svelte
+++ src/MainRight.svelte
@@ -1,117 +1,26 @@
 <script>
+    import { jData } from './store.js'
+    import { addChatWindow } from './ChatContainer.svelte'
+    $: friends = $jData.friends
 
+    const showChatWindow = () => {
+        addChatWindow()
+    }
 </script>
 
 <!-- ###################################### -->
 
 <div id="a" class="main-right">
 
-    <div class="mini-profile">
-    <img src="https://source.unsplash.com/random/50x55" alt="">
-    <div>
-        <div>Alfred Lin</div>
-        <div>Partner, Sequoia Capital</div>
-    </div>
-    </div>
-
-    <div class="mini-profile">
-    <!-- <img src="images/2.jpg" alt=""> -->
-    <img src="https://source.unsplash.com/random/50x56" alt="">
-    <div>
-        <div>Kais Khimji</div>
-        <div>San Francisco Bay Area</div>
-    </div>
-    </div>
-
-    <div class="mini-profile">
-    <!-- <img src="images/3.jpg" alt=""> -->
-    <img src="https://source.unsplash.com/random/50x57" alt="">
-    <div>
-        <div>Jeff Grimes</div>
-        <div>Google PM</div>
-    </div>
-    </div>      
-
-    <div class="mini-profile">
-    <!-- <img src="images/4.jpg" alt=""> -->
-    <img src="https://source.unsplash.com/random/50x58" alt="">
-    <div>
-        <div>Sarah Wang</div>
-        <div>Partner at Andreessen Horowitz</div>
-    </div>
-    </div>
-
-    <div class="mini-profile">
-    <!-- <img src="images/5.jpg" alt=""> -->
-    <img src="https://source.unsplash.com/random/50x59" alt="">
-    <div>
-        <div>Jess Lee</div>
-        <div>Co-Founder & CEO of Polyvore</div>
-    </div>
-    </div>
-
-    <div class="mini-profile">
-    <!-- <img src="images/6.jpg" alt=""> -->
-    <img src="https://source.unsplash.com/random/50x60" alt="">
-    <div>
-        <div>Ravi Gupta</div>
-        <div>Equity Investment Professional</div>
-    </div>
-    </div>  
-
-    <div class="mini-profile">
-    <!-- <img src="images/7.jpg" alt=""> -->
-    <img src="https://source.unsplash.com/random/50x61" alt="">
-    <div>
-        <div>Rasmus Bjerngaard</div>
-        <div>Co-Founder CEO at Nextfood</div>
-    </div>
-    </div>
-
-    <div class="mini-profile">
-    <!-- <img src="images/8.jpg" alt=""> -->
-    <img src="https://source.unsplash.com/random/50x62" alt="">
-    <div>
-        <div>Peter Green</div>
-        <div>VP HR EMEA at The Coca-Cola Company</div>
-    </div>
-    </div>
-
-    <div class="mini-profile">
-    <!-- <img src="images/9.jpg" alt=""> -->
-    <img src="https://source.unsplash.com/random/50x51" alt="">
-    <div>
-        <div>Kristina Stengelshøj Rasmussen</div>
-        <div>Head of Marketing Denmark, at The Coca-Cola Company</div>
-    </div>
-    </div>        
-
-    <div class="mini-profile">
-    <!-- <img src="images/10.jpg" alt=""> -->
-    <img src="https://source.unsplash.com/random/50x52" alt="">
-    <div>
-        <div>Tina Mose</div>
-        <div>Chief HR Officer</div>
-    </div>
-    </div>
-
-    <div class="mini-profile">
-    <!-- <img src="images/11.jpg" alt=""> -->
-    <img src="https://source.unsplash.com/random/50x53" alt="">
-    <div>
-        <div>Thomas Kolber</div>
-        <div>Transformation | HR Strategy</div>
-    </div>
-    </div>
-
-    <div class="mini-profile">
-    <!-- <img src="images/12.jpg" alt=""> -->
-    <img src="https://source.unsplash.com/random/250x254" alt="">
-    <div>
-        <div>Marlene Nyholm Voss</div>
-        <div>Senior Vice President, CHRO at SimCorp</div>
-    </div>
-    </div>    
+    {#each friends as friend}
+        <div class="mini-profile" on:click="{showChatWindow}">
+            <img src="https://source.unsplash.com/random/50x55" alt="">
+            <div>
+                <div>{ friend.name }</div>
+                <div>STATUS</div>
+            </div>
+        </div> 
+    {/each}  
 
 </div>
 
@@ -138,6 +47,10 @@ div.main-right{
   /* overflow: hidden; */
   overflow-y: scroll;
   z-index: 1;
+}
+
+div.mini-profile {
+    cursor: pointer;
 }
 
 </style>
